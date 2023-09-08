67d7244ede6f0bc5fcca965defc89ace82a4e6be
Travis <trapcoleman@gmail.com>
2023-09-08 01:24:40 -0400

    Forms corrected to add to database

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #path('login/', views.login_user, name='login'),
+    # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('player/<int:pk>', views.player_info, name='player'),
     path('add_scorecard/', views.add_scorecard, name='add_scorecard'),
     path('view_scorecard/', views.view_scorecard, name='view_scorecard'),
+    # path('delete_scorecard/', views.delete_scorecard, name='delete_scorecard'),
 ]
